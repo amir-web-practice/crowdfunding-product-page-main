662d197d25bdc45f35be37e14f3fe62106170e1c
AmirMardan-prog <amirmardan.prog@gmail.com>
2021-08-25 16:53:19 -0400

    fix the bug gor menu

--- script/app.js
+++ script/app.js
@@ -47,3 +47,19 @@ document.querySelector(".menu-logo").addEventListener('click', function(){
   }
 
 })
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.querySelector(".menu .menu-desktop").style.visibility= "visible"
+  } else {
+    document.querySelector(".menu .menu-desktop").style.visibility= "hidden"
+    document.querySelector(".menu .menu-mobile").style.visibility= "hidden"
+    document.querySelector(".menu .menu-logo").style.backgroundImage = "url('../images/icon-hamburger.svg')";
+
+  }
+}
+
+var x = window.matchMedia("(min-width: 429px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
